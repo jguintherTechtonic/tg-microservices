fded3c31a6070935571cf3ac33b7644574ae1553
jguintherTechtonic <jacob.guinther@techtonic.com>
2020-01-22 16:31:24 -0700

    init commit

---
+++ tg-config-server/src/test/java/com/techtonic/microservices/config/PetclinicConfigServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.techtonic.microservices.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicConfigServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
